f1c777985469078f00caf1c438bacb10b28b0fcf
IvanSantana <iam.ivansantana@gmail.com>
2021-02-22 14:01:06 -0600

    LetrasApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Cancion from './components/Cancion';
+import Footer from './components/Footer';
+import Formulario from './components/Formulario';
+import Info from './components/Info';
+
 
 function App() {
+
+  //Definir el state
+  const [busquedaLetra,guardarBusquedaLetra]=useState({});
+  const [letra,guardarLetra]=useState('');
+  const [info,guardarInfo]=useState({});
+
+  useEffect(() => {
+    if(Object.keys(busquedaLetra).length === 0) return;
+
+    const consultarAPIletra = async()=>{
+      const {artista,cancion}=busquedaLetra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+      const [letra, informacion] = await Promise.all([
+        Axios(url),
+        Axios(url2)
+      ]);
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]); 
+      
+      
+    }
+    consultarAPIletra();
+    
+  }, [busquedaLetra]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Formulario 
+        guardarBusquedaLetra={guardarBusquedaLetra}
+      />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col md-6">
+            <Info 
+              info={info}
+            />
+          </div>
+          <div className="col md-6">
+            <Cancion 
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
